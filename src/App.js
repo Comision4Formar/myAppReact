5d7006c205d3ec6c63f05a81e4f8c2912239824f
EricFormar <53233372+EricM76@users.noreply.github.com>
2021-03-26 12:26:08 -0300

    avancessss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Fragment } from 'react';
 import './App.css';
-import Main from './components/Main';
-import SideBar from './components/SideBar';
+import Footer from './components/Footer';
+import Main from './components/Main/Main';
+import SideBar from './components/SideBar/SideBar';
 
 function App() {
   return (
     <div id="wrapper">
     <SideBar/>
-    <div id="content-wrapper" class="d-flex flex-column">
+    <div id="content-wrapper" className="d-flex flex-column">
       <Main/>
+      <Footer/>
     </div>
     </div>
   );
